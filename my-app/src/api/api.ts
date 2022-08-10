d8ddf5d8a05f335a15fd623042230271389dcffb
vadchurs90 <vadchurs90@gmail.com>
2022-08-10 13:36:36 +0300

    add a comment in api file

--- my-app/src/api/api.ts
+++ my-app/src/api/api.ts
@@ -1,19 +1,22 @@
 import { PayloadType } from "./../App";
 
-// здесь создал имитацию отправки пост запроса на сервер, так как 
+// здесь создал имитацию отправки пост запроса на сервер, для того чтобы получить 
+//статус error необходимо в текстовом поле отправить только числовые значения без букв
 
 export const fakeApi = {
   postFakeApi(payload: PayloadType): Promise<any> {
     const promise = new Promise((res, rej) => {
-
-        if(!Number(payload.messageText)){
-            setTimeout(
-                () => res({ status: "succes", message: "Ваше сообщение отправлено" }),
-                3000
-              );
-        } else {
-            setTimeout(() => rej({ status: "error", message: "Произошла ошибка"}), 3000);
-        }
+      if (!Number(payload.messageText)) {
+        setTimeout(
+          () => res({ status: "succes", message: "Ваше сообщение отправлено" }),
+          3000
+        );
+      } else {
+        setTimeout(
+          () => rej({ status: "error", message: "Произошла ошибка" }),
+          3000
+        );
+      }
     });
     return promise;
   },
